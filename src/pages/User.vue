58565d86bd105346d46a9acc1492c7e84d9435c9
Li-Win-AMD <lichengyang@aliyun.com>
2017-09-03 03:34:59 +0800

    Update Drafts

---
+++ src/pages/User.vue
@@ -0,0 +1,21 @@
+/*
+ * @Author: Li 
+ * @Date: 2017-09-03 03:34:32 
+ * @Last Modified by:   Li-1700x 
+ * @Last Modified time: 2017-09-03 03:34:32 
+ */
+<template>
+  <div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
