bc5b4f2fc3e8fff304262e4ec8597e6cff5ac77f
cyancity-1700x <lichengyang@aliyun.com>
2017-09-14 12:22:10 +0800

    move on

--- src/components/common/Register.vue
+++ src/components/common/Register.vue
@@ -3,11 +3,16 @@
     <x-header title="注册"></x-header>
     <group>
       <x-input title="手机号" type="text" v-model="userInfo.phone" placeholder="请输入手机号码"></x-input>
-      <x-input title="验证码" type="text" v-model="userInfo.captcha" placeholder="请输入手机号码"></x-input>
+      <div class="captcha">
+        <x-input title="验证码" type="text" v-model="userInfo.captcha" placeholder="请输入手机号码"></x-input>
+        <x-button mini="true">
+          发送验证码
+        </x-button>
+      </div>
       <x-input title="密码" type="text" v-model="userInfo.password " placeholder="请输入手机号码"></x-input>
     </group>
-    <input type="radio" placeholder="协议">
-    <x-button text="注册" type="primary" @click.native="register"></x-button>
+    <input type="checkbox" placeholder="协议" ref="protocol" checked="false" @click="isAgree">
+    <x-button text="注册" type="primary" @click.native="register" disabled="isRegister"></x-button>
   </div>
 </template>
 
@@ -26,19 +31,34 @@ export default {
       userInfo: {
         phone: '',
         captcha: '',
-        password: ''
+        password: '',
+        isRegister: true
       }
     }
   },
   methods: {
     register () {
-      this.$store.dispatch('register', this.userInfo)
-      this.$router.push('/choose')
+      if (this.isRegister === false) {
+        this.$store.dispatch('register', this.userInfo)
+        this.$router.push('/choose')
+      } else {
+        console.log('error')
+      }
+    },
+    isAgree () {
+      this.$refs.protocol.checked
     }
   }
 }
 </script>
 
-<style>
-
+<style lang="stylus" scoped>
+@import '../../styles/common'
+.captcha
+  display: flex
+  .send
+    border-radius 5px
+    border 1px solid #fff
+    background pr-blue
+    width 60px
 </style>
