dae4f88ce935f7a25866f6ab0b109b971cc42a63
cyancity-1700x <lichengyang@aliyun.com>
2017-09-18 11:28:48 +0800

    move on

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,15 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import user from './modules/user'
+import course from './modules/course'
 import getters from './getters'
 
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
   modules: {
-    user
+    user,
+    course
   },
   getters
 })
